9d05bf90057905721b674ec8a84db9cb11a19539
Fedot B Pozdnyakov 9* <9@u9.ru>
2014-11-26 13:56:42 +0300

    Параметры задания сортировки вывда листа сделаны

--- var/ui/mf2_catalogue_list/mf2_catalogue_list.ui.php
+++ var/ui/mf2_catalogue_list/mf2_catalogue_list.ui.php
@@ -22,16 +22,17 @@ class ui_mf2_catalogue_list extends user_interface
 		$data = array();
 		// Шаблон
 		$args = $this->get_args();
+		$sort = $this->get_args('sort','id');
+		$dir = $this->get_args('dir','asc');
 		$template = $this->get_args('template', 'default.html');
 		$ui = user_interface::get_instance('mf2_catalogue_nav');
 		$scope = $ui->get_scope();
 		$item_id = $ui->item_id;
 		$di = data_interface::get_instance('mf2_catalogue_list');
-		$di->push_args(array('scope'=>$scope));
+		$di->set_args(array('scope'=>$scope,'sort'=>$sort,'dir'=>$dir));
 		$data['records'] = $di->get_list();
 		$data['basket'] = $_SESSION['mf2_cart'];
 		$data['current_node'] = $ui->location_data['current_node'];
-		$di->pop_args();
 		return $this->parse_tmpl($template,$data);
 	}
 //9*  списко реагирующий на ходяий параметр search  для поиска по каталогу
@@ -39,9 +40,11 @@ class ui_mf2_catalogue_list extends user_interface
 	{
 		$template = $this->get_args('template', 'default.html');
 		$di = data_interface::get_instance('mf2_catalogue_list');
+		$sort = $this->get_args('sort','id');
+		$dir = $this->get_args('dir','asc');
 		$search = request::get('search');
 		$cat = request::get('cat');
-		$di->push_args(array('search'=>$search,'cat'=>$cat));
+		$di->push_args(array('search'=>$search,'cat'=>$cat,'sort'=>$sort,'dir'=>$dir));
 		$data['records'] = $di->get_list(true);
 		$data['basket'] = $_SESSION['mf2_cart'];
 		$di->pop_args();
