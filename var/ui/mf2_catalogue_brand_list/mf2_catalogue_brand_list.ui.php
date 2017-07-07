fc8fbd845577c98ca2d6ad46543acb19d8fad83d
Fedot B Pozdnyakov 9* <9@u9.ru>
2017-07-07 18:12:11 +0300

    добавлен метод списка характеристики по заданному ID

--- var/ui/mf2_catalogue_brand_list/mf2_catalogue_brand_list.ui.php
+++ var/ui/mf2_catalogue_brand_list/mf2_catalogue_brand_list.ui.php
@@ -31,6 +31,7 @@ class ui_mf2_catalogue_brand_list extends user_interface
 		return $this->parse_tmpl($template,$data);
 	}
 
+	//производители по заданной группе
 	public function pub_by_group()
 	{
 		$data = array();
@@ -48,6 +49,23 @@ class ui_mf2_catalogue_brand_list extends user_interface
 		$sql = "select m.*,f.real_name,f.file_type from m2_manufacturer_in_groups g left join m2_manufacturers m on g.item_id = m.id left join m2_manufacturer_files f on f.item_id = m.id where g.group_id = $group group by m.id";
 		$data['records'] = $di->_get($sql)->get_results();
 		$data['PAGE_ID'] = PAGE_ID;
+		$data['req'] = request::get();
+		return $this->parse_tmpl($template,$data);
+	}
+
+	public function pub_char_list()
+	{
+		$char_id =  $this->get_args('char_id',0);
+		if(!($char_id >0))
+		{
+			return '';
+		}
+		$template = $this->get_args('template', 'char_list.html');
+		$sort = $this->get_args('sort','title');
+		$dir = $this->get_args('dir','asc');
+		$di = data_interface::get_instance('m2_chars_types');
+		$ns = new nested_sets($di);
+		$data['records'] = $ns->get_childs($char_id);
 		$data['req'] = request::get();
 		return $this->parse_tmpl($template,$data);
 	}
