e89f0b37fbcab14c231c7ab62af36c475eb194a0
Fedot B Pozdnyakov 9* <9@u9.ru>
2019-12-24 01:55:04 +0300

    новый di для массовой индексации по ивенту окончания индексации в m2_item_indexer

---
+++ var/di/mf2_mass_reindex.di.php
@@ -0,0 +1,55 @@
+<?php
+/**
+*
+* @author	Fedot B Pozdnyakov <9@u9.ru>  
+* @package	SBIN Diesel
+*/
+class di_mf2_mass_reindex extends data_interface
+{
+	public $title = 'реиндекс';
+	/**
+	* @var	string	$cfg	Имя конфигурации БД
+	*/
+	protected $cfg = 'localhost';
+	
+	/**
+	* @var	string	$db	Имя БД
+	*/
+	protected $db = 'db1';
+
+
+	protected $cart_data = array();
+	/**
+	* @var	array	$fields	Конфигурация таблицы
+	*/
+	public $fields = array(
+	);
+	
+	public function __construct ()
+	{
+	    // Call Base Constructor
+	    parent::__construct(__CLASS__);
+	}
+	
+	public function reindex()
+	{
+		$sql = 'select * from m2_item_indexer';
+		$res = $this->_get($sql)->get_results();
+		$di = data_interface::get_instance('m2_chars_in_category');
+		$di->recache($res);
+		$di = data_interface::get_instance('m2_category_manufacturers');
+		$di->recache($res);
+		$di = data_interface::get_instance('m2_category_price');
+		$di->recache($res);
+		unset($res);
+		return ;
+	}
+	public function _listeners()
+	{
+		return array(
+			array('di' => 'm2_item_indexer', 'event' => 'after_reindex', 'handler' => 'reindex'),
+		);
+	}
+
+}
+?>
